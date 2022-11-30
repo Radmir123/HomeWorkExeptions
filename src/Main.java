3933a5e88be3fd7013fdda254c312340d06e6cd2
Радмир <sch.radmir@yandex.ru>
2022-11-30 22:14:25 +0300

    проверьте пожалуйста ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+      boolean example = Cheks.CheckMethod("java_skypro.go",
+              "D_1hWiKjjP_9",
+              "D_1hWiKjjP_9");
+      if (example){
+          System.out.println("логин и пароль корректен");
+      }else {
+          System.out.println("Исправь или логин или пароль");
+      }
 
-        System.out.println("Hello world!");
+
+
+
     }
 }
